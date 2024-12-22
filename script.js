7e94cd76d6ceebd6fbe1c9dc424daa545d68da1c
Marchanyn <sergey.kozlov.0404@mail.ru>
2024-12-22 22:10:33 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Документ загружен и готов к работе!");
+
+    const greeting = () => {
+        alert("Добро пожаловать на мой сайт о книге!");
+    };
+
+    greeting();
+});
